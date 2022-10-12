c657fcab994192f574398021f062cd99787eee02
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-12 21:05:20 +0300

    drinks

--- drinks/drinkers/views.py
+++ drinks/drinkers/views.py
@@ -4,4 +4,9 @@ from drinkers.models import Drink
 from drinkers.serializers import DrinkSerializer
 
 def drink_list(request):
-    pass
+    # get all the drinks
+    # serialize the drinks
+    # return a JSON response
+    drinks = Drink.objects.all()
+    serializer = DrinkSerializer(drinks, many=True)
+    return JsonResponse(serializer.data,safe=False)
