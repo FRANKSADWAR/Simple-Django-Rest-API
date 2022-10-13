8dede0a596a59626f0183ef79d0e689b2dc0f7e9
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-13 10:40:52 +0300

    consume

--- drinks/drinkers/views.py
+++ drinks/drinkers/views.py
@@ -1,15 +1,23 @@
+from symbol import pass_stmt
 from django.shortcuts import render
 from django.http import JsonResponse
 from drinkers.models import Drink
 from drinkers.serializers import DrinkSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
 from rest_framework import status 
+from rest_framework import schemas
 
-from drinks import drinkers
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
+
+
+
+
+
 
 @api_view(['GET','POST'])
-def drink_list(request):
+def drink_list(request, format=None):
 
     # get all the drinks
     # serialize the drinks
@@ -19,7 +27,7 @@ def drink_list(request):
     if request.method == "GET":
         drinks = Drink.objects.all()
         serializer = DrinkSerializer(drinks, many=True)
-        return JsonResponse({'drinks':serializer.data})
+        return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
 
     ## the post view endpoint
     if request.method == "POST":
@@ -27,6 +35,22 @@ def drink_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
- 
-def drink_view(request,id):
-    pass
+
+@api_view(['GET','PUT','DELETE']) 
+def drink_detail(request,id, format=None):
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist :
+        return Response(status= status.HTTP_404_NOT_FOUND)    
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)              
+    elif request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
